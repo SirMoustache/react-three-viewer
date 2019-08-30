a2ecd022233e60dc021488647aa3e91e4cc688f7
SirMoustache <sethsleerk@gmail.com>
2019-08-30 13:42:44 +0300

    test array utills

--- src/utils/fileUtils.ts
+++ src/utils/fileUtils.ts
@@ -47,3 +47,16 @@ export const getFileParseResultType = (extension: string) => {
 
   return extensionParseResultType[extension] || 'string';
 };
+
+export const ab2str = (buf: ArrayBuffer) => {
+  return String.fromCharCode.apply(null, new Uint16Array(buf));
+};
+
+function str2ab(str: any) {
+  var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
+  var bufView = new Uint16Array(buf);
+  for (var i = 0, strLen = str.length; i < strLen; i++) {
+    bufView[i] = str.charCodeAt(i);
+  }
+  return buf;
+}
